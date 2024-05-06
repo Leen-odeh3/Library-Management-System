ca76c00bab5e4edb98e8e668b5a5127ca7716b86
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-06 16:23:15 +0300

    3. BorrowingFrequency

---
+++ Scripts/BorrowingFrequency.sql
@@ -0,0 +1,13 @@
+WITH BorrowerFrequency AS (
+    SELECT
+        BorrowerID,
+        COUNT(*) AS BorrowingFrequency,
+        ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS BorrowerRank
+    FROM Library.Loans
+    GROUP BY BorrowerID
+)
+SELECT
+    BorrowerID,
+    BorrowingFrequency,
+    BorrowerRank
+FROM BorrowerFrequency;
