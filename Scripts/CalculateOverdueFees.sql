8d3356dd43eab1e70d69b7eb4913a87f6b16b4b3
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-07 10:10:07 +0300

    6. Database Function

---
+++ Scripts/CalculateOverdueFees.sql
@@ -0,0 +1,37 @@
+CREATE OR ALTER FUNCTION fn_CalculateOverdueFees
+(
+    @LoanID INT
+)
+RETURNS DECIMAL(10, 2)
+AS BEGIN
+    DECLARE @DueDate DATE;
+    DECLARE @DateReturned DATE;
+    DECLARE @OverdueDays INT;
+    DECLARE @OverdueFee DECIMAL(10, 2);
+
+    SELECT @DueDate = DueDate, @DateReturned = DateReturned
+    FROM Library.Loans
+    WHERE LoanID = @LoanID;
+
+    SET @OverdueDays = DATEDIFF(DAY, @DueDate, COALESCE(@DateReturned, GETDATE()));
+
+    IF @OverdueDays > 0
+    BEGIN
+        IF @OverdueDays <= 30
+        BEGIN
+            SET @OverdueFee = @OverdueDays * 1.00;
+        END
+        ELSE
+        BEGIN
+            SET @OverdueFee = (30 * 1.00) + ((@OverdueDays - 30) * 2.00);
+        END
+    END
+    ELSE
+    BEGIN
+        SET @OverdueFee = 0.00; 
+    END
+
+    RETURN @OverdueFee;
+	End
+
+
