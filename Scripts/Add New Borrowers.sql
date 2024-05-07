39788f854cb85c110f4455ce57d133a03b2e8b72
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-07 09:56:51 +0300

    Add New Borrowers

---
+++ Scripts/Add New Borrowers.sql
@@ -0,0 +1,22 @@
+CREATE OR ALTER PROCEDURE sp_AddNewBorrower
+    @FirstName NVARCHAR(50), 
+    @LastName NVARCHAR(50), 
+    @Email NVARCHAR(255), 
+    @DateOfBirth DATE, 
+    @MembershipDate DATE,
+    @NewBorrowerID INT OUT
+AS BEGIN
+    SET NOCOUNT ON;
+
+    IF EXISTS(SELECT 1 FROM Library.Borrowers WHERE Email = @Email)
+    BEGIN
+        THROW 50000, 'Email already exists.', 1;
+    END
+    ELSE
+    BEGIN
+        INSERT INTO Library.Borrowers (FirstName, LastName, Email, DateOfBirth, MembershipDate)
+        VALUES (@FirstName, @LastName, @Email, @DateOfBirth, @MembershipDate);
+
+        SET @NewBorrowerID = SCOPE_IDENTITY();
+    END
+END;
