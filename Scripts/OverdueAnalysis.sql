aa991150b114181ce6ab729644250d8d92462bd3
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-08 11:14:32 +0300

    8. Overdue Analysis

---
+++ Scripts/OverdueAnalysis.sql
@@ -0,0 +1,30 @@
+-- 8. Overdue Analysis
+DECLARE @OverdueDays INT = 10;
+WITH OverdueBooks AS (
+    SELECT
+        BookID,
+        BorrowerID,
+		DateBorrowed,
+        DueDate,
+        DateReturned,
+        OverdueDays = DATEDIFF(DAY, DueDate, COALESCE(DateReturned, GETDATE()))
+    FROM Circulation.Loans
+    WHERE DATEDIFF(DAY, DueDate, COALESCE(DateReturned, GETDATE())) > @OverdueDays
+)
+SELECT
+    k.BookID,
+    k.Title,
+    k.Author,
+    w.BorrowerID,
+    w.FirstName,
+    w.LastName,
+    w.Email,
+	o.DateBorrowed,
+    o.DueDate,
+    o.DateReturned,
+    o.OverdueDays
+FROM OverdueBooks o
+JOIN Library.Books k ON o.BookID = k.BookID
+JOIN Library.Borrowers w ON o.BorrowerID = w.BorrowerID
+ORDER BY OverdueDays DESC, w.FirstName;
+
