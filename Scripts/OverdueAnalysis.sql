dc554d176277ab08cbb87b6b4252eb36df9814d2
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-09 13:47:16 +0300

    fix format

--- Scripts/OverdueAnalysis.sql
+++ Scripts/OverdueAnalysis.sql
@@ -12,20 +12,20 @@ WITH OverdueBooks AS (
     FROM Library.Loans
     WHERE DATEDIFF(DAY, DueDate, COALESCE(DateReturned, GETDATE())) > @OverdueDays
 )
-SELECT
-    k.BookID,
-    k.Title,
-    k.Author,
-    w.BorrowerID,
-    w.FirstName,
-    w.LastName,
-    w.Email,
-	o.DateBorrowed,
-    o.DueDate,
-    o.DateReturned,
-    o.OverdueDays
-FROM OverdueBooks o
-JOIN Library.Books k ON o.BookID = k.BookID
-JOIN Library.Borrowers w ON o.BorrowerID = w.BorrowerID
-ORDER BY OverdueDays DESC, w.FirstName;
+    SELECT
+        k.BookID,
+        k.Title,
+        k.Author,
+        w.BorrowerID,
+        w.FirstName,
+        w.LastName,
+        w.Email,
+        o.DateBorrowed,
+        o.DueDate,
+        o.DateReturned,
+        o.OverdueDays
+    FROM OverdueBooks o
+    JOIN Library.Books k ON o.BookID = k.BookID
+    JOIN Library.Borrowers w ON o.BorrowerID = w.BorrowerID
+    ORDER BY OverdueDays DESC, w.FirstName;
 
