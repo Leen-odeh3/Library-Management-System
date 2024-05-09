a396142ea229c3ef17a9b8ba3c88b656e13498bb
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-09 11:46:21 +0300

    1. List of Borrowed Books

--- Scripts/ListofBorrowedBooks.sql
+++ Scripts/ListofBorrowedBooks.sql
@@ -1,5 +1,5 @@
 -- 1. List of Borrowed Books
-
+-- Retrieve all books borrowed by a specific borrower, including those currently unreturned.
 declare @BorrowerId int = 5;
 
 SELECT  Library.Books.Title, Books.Author, Loans.DateBorrowed, Loans.DueDate
