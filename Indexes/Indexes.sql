959a6df4b6ed36a310f00c48803dc8cb2bff547b
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-16 15:17:33 +0300

    Add Changes

--- Indexes/Indexes.sql
+++ Indexes/Indexes.sql
@@ -1,43 +1,31 @@
 -- Books 
 
 CREATE NONCLUSTERED INDEX idx_Books_Covering 
-ON Library.Books(BookID, Author)
-INCLUDE (Title);
+ON Library.Books(BookID, Title, CurrentStatus);
 
 -- Loans 
 
-CREATE NONCLUSTERED INDEX idx_Loans_BookID_DateBorrowed 
-ON Library.Loans(BookID, DateBorrowed);
-
 CREATE NONCLUSTERED INDEX idx_Loans_BorrowerID_DateBorrowed 
-ON Library.Loans(BorrowerID, DateBorrowed);
+ON Library.Loans(BorrowerID, DateBorrowed)
+INCLUDE (BookID, DateReturned)
+WHERE DateReturned IS NULL;
 
 CREATE NONCLUSTERED INDEX idx_Loans_ActiveLoans 
 ON Library.Loans (BorrowerID, DateReturned)
 INCLUDE (BookID) 
 WHERE DateReturned IS NULL;
 
-CREATE NONCLUSTERED INDEX idx_Loans_DateBorrowed 
-ON Library.Loans(DateBorrowed);
-
-CREATE NONCLUSTERED INDEX idx_Loans_DueDate_DateReturned 
-ON Library.Loans(DueDate, DateReturned)
-INCLUDE (BookID, BorrowerID, DateBorrowed);
+CREATE NONCLUSTERED INDEX idx_Loans_BookID_DateBorrowed 
+ON Library.Loans(BookID, DateBorrowed);
 
 
 -- BookGenres 
 
-CREATE NONCLUSTERED INDEX idx_BookGenres_BookID 
-ON Library.BookGenres(BookID);
-
-CREATE NONCLUSTERED INDEX idx_BookGenres_GenreID 
-ON Library.BookGenres(GenreID);
+CREATE NONCLUSTERED INDEX idx_BookGenres_BookID_GenreID 
+ON Library.BookGenres(BookID, GenreID);
 
 
 -- Borrowers
-
-CREATE NONCLUSTERED INDEX idx_Borrowers_DateOfBirth 
-ON Library.Borrowers(DateOfBirth);
 
 CREATE NONCLUSTERED INDEX idx_Borrowers_Covering 
 ON Library.Borrowers(BorrowerID)
